0ae70ff7370b206f851b8d764e8cfb40773bf1dc
nicolas-estevez <nicolas.estevez@infodeclat.com>
2023-05-28 13:10:50 -0500

    🎉 feat(reactRouterExample): añadir configuración de ESLint, .gitignore, index.html, package.json, vite.svg y App.jsx Se agregó la configuración de ESLint para mantener un código limpio y consistente. Se agregó un archivo .gitignore para evitar que se suban archivos innecesarios al repositorio. Se agregó un archivo index.html para servir la aplicación React. Se agregó un archivo package.json para manejar las dependencias y scripts de la aplicación. Se agregó un archivo vite.svg para mostrar el logo de Vite. Se agregó un componente App.jsx para mostrar un mensaje de "Hello World" en la aplicación.
    🎨 style(react.svg): agregar salto de línea al final del archivo
    ✨ feat(navbar.jsx): crear componente Navbar y agregar enlaces a las páginas de inicio, nosotros y contacto
    ✨ feat(main.jsx): agregar BrowserRouter y renderizar componente App dentro de él
    ✨ feat(vite.config.js): agregar plugin react a la configuración de Vite
    Se agregó un salto de línea al final del archivo react.svg para cumplir con las convenciones de estilo. Se creó el componente Navbar y se agregaron enlaces a las páginas de inicio, nosotros y contacto. Se agregó BrowserRouter y se renderizó el componente App dentro de él en main.jsx. Se agregó el plugin react a la configuración de Vite en vite.config.js.

---
+++ reactRouterExample/src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/navbar";
+const App = () => { 
+    return <>
+        <Navbar />
+        <div className="App"> 
+            <h1>Hello World</h1> 
+        </div> 
+    </>;
+}
+
+export default App;
